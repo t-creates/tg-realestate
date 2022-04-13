8cf7889085a1ae000a906d743932593e0e2eec0e
Travis Geislinger <travisgeislinger@Traviss-MacBook-Pro.local>
2022-04-13 16:31:14 -0600

    Deployment bug fix

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,6 +5,7 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
+import 'tachyons';
 
 import DefaultImage from '../assets/images/house.jpg';
 
